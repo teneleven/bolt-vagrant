fb174ed0c6a2dc3f4f5beb848f356575c21aeae3
Michael Mackus <michaelmackus@gmail.com>
2015-09-03 16:24:50 -0700

    Basic grunt setup with CKEDITOR overrides

--- vagrant/puphpet/files/exec-once/bolt-setup.sh
+++ vagrant/puphpet/files/exec-once/bolt-setup.sh
@@ -3,10 +3,8 @@
 cd /var/www
 cp app/config/config.yml.dist app/config/config.yml
 
-composer install --prefer-dist
-
 if [ ! -d "app/cache" ]; then
     mkdir app/cache
 fi
 
-sudo chmod -R 755 web/files/ app/database/ app/cache/ app/config/ web/theme/ web/extensions/
+chmod -R 755 web/files/ app/database/ app/cache/ app/config/ web/theme/ web/extensions/
