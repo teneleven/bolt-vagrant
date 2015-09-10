bd4b8d68ef9aa61f7ff3a1f91b7a8c1ae698755a
Michael Mackus <michaelmackus@gmail.com>
2015-09-10 15:40:30 -0700

    Fix stripping of icons in FF on backspace

--- app/src/js/ckeditor-config.js
+++ app/src/js/ckeditor-config.js
@@ -1,4 +1,5 @@
 if (typeof CKEDITOR != 'undefined') {
-    // don't remove empty <i> tags (icons)
-    CKEDITOR.dtd.$removeEmpty.i = false;
+    // don't remove empty <i> or <span> tags (icons)
+    CKEDITOR.config.protectedSource.push(/<i[^>]*><\/i>/g);
+    CKEDITOR.config.protectedSource.push(/<span[^>]*><\/span>/g);
 }
